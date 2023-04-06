06779c36ee797a6d528453d707095180c42704e2
DiegoxK <diego.stv@outlook.es>
2023-04-06 13:27:07 -0500

    Start template

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./style.css";
-import javascriptLogo from "/javascript.svg";
-import viteLogo from "/vite.svg";
+import OpenAILogo from "/openai.png";
 import { Configuration, OpenAIApi } from "openai";
 
 const openai = new OpenAIApi(
@@ -24,15 +23,12 @@ openai
 
 document.querySelector("#app").innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    <a href="https://platform.openai.com/" target="_blank">
+      <img src="${OpenAILogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
+    <h1>Hello GPT!</h1>
     <p class="read-the-docs">
-      aaaaaaaaa
+      Chat Here!
     </p>
   </div>
 `;
